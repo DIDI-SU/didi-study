fc7058fe13a584e9ced7e9342f038bc62d916a01
DIDI-SU <jisoodidi86@gmail.com>
2022-07-04 15:15:27 +0900

    feat: Add todo 기능추가

--- todo-app/pages/index.js
+++ todo-app/pages/index.js
@@ -1,4 +1,36 @@
+import { useState } from "react";
+
 const Home = () => {
-  return <div className="text-4xl"> home</div>;
+  const [todoItem, setTodoItem] = useState("");
+  const [items, setItmes] = useState([
+    "무언가사기",
+    "무언가사기2",
+    "무언가사기3",
+  ]);
+  const handleAdd = () => {
+    setItmes((prve) => [...prve, todoItem]);
+    setTodoItem("");
+  };
+
+  return (
+    <div>
+      <h1>ToDoApp</h1>
+      <div>
+        <input
+          type="text"
+          value={todoItem}
+          onChange={(e) => setTodoItem(e.target.value)}
+        />
+        <button type="button" onClick={() => handleAdd()}>
+          Add
+        </button>
+      </div>
+      <ul>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default Home;
