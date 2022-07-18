6b85b878ebbbfd147b3edbb444a956ed82910047
DIDI-SU <jisoodidi86@gmail.com>
2022-07-18 17:57:36 +0900

    feat:TS 공부

---
+++ nextjs-practice/lib/posts.js
@@ -0,0 +1,29 @@
+import path from "path";
+import matter from "gray-matter";
+const fs = require("fs");
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export const getSortedPostsData = () => {
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, "");
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readdirSync(fullPath, "utf8");
+    const matterResult = matter(fileContents);
+    return {
+      id,
+      ...matterResult.data,
+    };
+  });
+
+  return allPostsData.sort(({ date: a }, { date: b }) => {
+    if (a < b) {
+      return 1;
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+};
---
+++ nextjs-practice/pages/index.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+
+export const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: { allPostsData },
+  };
+};
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData &&
+            allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
