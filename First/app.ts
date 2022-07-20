ad8906ed263cf8dc44208a1eef2596e9d1a7e89a
DIDI-SU <jisoodidi86@gmail.com>
2022-07-20 10:36:56 +0900

    feat:1강 완료

--- First/app.ts
+++ First/app.ts
@@ -1,53 +1,43 @@
-//union
-// 현재 아래의 함수는  현재 두개의 숫자 자료형 인자만 가능한
-// 함수 입니다. 만약 문자열과 함께 사용하고 싶은 경우에는 어떻게 해주어야할까요?
-const add = (n1: number, n2: number) => {
-  const result = n1 + n2;
-  return result;
-};
+//unknown
+//현재 우리는 유저가 어떤 값을 입력할지 모르므로 unknown이라는 타입을
+// 변수에 지정해 주었습니다
+// let userInput: unknown;
 
-// const combine = (input1: number | string, input2: number | string) => {
-//   let result;
-//   if (typeof input1 === "number" && typeof input2 === "number") {
-//     result = input1 + input2;
-//   } else {
-//     result = input1.toString() + input2.toString();
-//   }
-//   return result;
-// };
-// const combineAge = combine(10, 15);
-// const combineName = combine("lee", 20);
+// userInput = 5;
+// userInput = "lee";
 
-// console.log(combineAge, combineName);
+//그리고 unknown의 경우에는 어떤 값을 넣어도 에러 없이 무사히 컴파일되는 것을 확인할 수 있습니다
+//자세히 생각해 보니 아마 변수에 아무런 타입을 지정해 주지 않은 상태에도
+//우리는 이와 같은 결과를얻을 수 있습니다
+//그렇다면  any와  unknown의 차이는 무엇일까요?
+//한번 문자열 자료형을 지니는 유저명 변수에 유저 인풋을 할당해 주니
+//에러가 발생했습니다  unknown은 string에 적용할수 없다구요
+let userInput: unknown;
+let userName: string;
+userInput = 5;
+userInput = "lee";
+// userName= userInput
+//반대로  any의 경우에는 가능합니다.  unknown은  any보다는 조금더
+// 제한적인 타입으로, 중간에 한번 변수의 자료형을 확인해 주어야 합니다
 
-//리터럴
-//정확한 값을 가지는 값, 예를 들어 상수 변수에 숫자를 지정한경우, 정확히 타입과 값이 정해져 있어
-//사용에 용이해집니다
-//리터럴은 주로 유니언 타입을 결합하는데 사용됩니다.
+if (typeof userInput === "string") {
+  userName = userInput;
+}
+//any를 사용하는 것보다 . unknown을 사용해주는 것이 타입을 확인하고
+// 에러를 방지하는 일등을 진행해 주기에 용이합니다
+// 물론 어떤 값이 올지 조금이라도 면확하다면  union을 사용해 주어야하지만요
 
-const combine = (
-  input1: Combinable,
-  input2: Combinable,
-  resultType: "as-number" | "as-text"
-) => {
-  let result;
-  if (resultType === "as-number") {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  //   if (resultType === "as-number") {
-  //     return +result;
-  //   } else {
-  //     return result.toString();
-  //   }
-};
-const combineAge = combine(10, 15, "as-number");
-const combineName = combine("lee", "20", "as-text");
-const combineString = combine("lee", "ji", "as-text");
+//절대 :never => 함수가 반환할수 있는 타입
+//이함수는 언제나 스크립트를 충돌해, 아무것도 반환하지 않습니다
+//즉 아무런 값을 출력하지 않습니다
+//그래서  never역시 가능합니다
+// function generateError(message: string, code: number): never {
+//   throw { message: message, errorCode: code };
+// }
 
-console.log(combineAge, combineName, combineString);
-
-//타입 별칭: type alias
-type Combinable = number | string;
+// const result = generateError("error!", 500);
+// console.log(result);
+// function add(num: number, num2: number): void {
+//   const result: number = num + num2;
+//   console.log(result);
+// }
